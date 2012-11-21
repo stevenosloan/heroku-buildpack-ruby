9641e471febaf4d95f8dfd0cce5b1c838ed6568d
Steven Sloan <stevenosloan@gmail.com>
2012-11-21 12:08:02 -0500

    identical to rack languagepack, for testing

--- lib/language_pack/middleman.rb
+++ lib/language_pack/middleman.rb
@@ -8,11 +8,11 @@ class LanguagePack::Middleman < LanguagePack::Ruby
   # detects if this is a Middleman app
   # @return [Boolean] true if it's a Middleman app
   def self.use?
-    super && gem_is_bundled?("middleman")
+    super && File.exist?("config.ru")
   end
 
   def name
-    "Ruby/Middleman"
+    "Ruby/Rack"
   end
 
   def default_config_vars
@@ -32,42 +32,12 @@ class LanguagePack::Middleman < LanguagePack::Ruby
     })
   end
 
-  def compile
-    super
-    # allow_git do
-    #   run_middleman_build_process
-    # end
-  end
-
 private
 
   # sets up the profile.d script for this buildpack
   def setup_profiled
     super
     set_env_default "RACK_ENV", "production"
-  end
-  
-  # check if a middleman:build rake task exists
-  # or run the default middleman build
-  def run_middleman_build_process
-
-    if rake_task_defined?("middleman:build")
-      task = "rake middleman:build"
-    else
-      puts "no middleman:build task detected"
-      task = "middleman build --clean"
-    end
-
-    require 'benchmark'
-
-    topic "Running: #{task}"
-    time = Benchmark.realtime { pipe("env PATH=$PATH:bin bundle exec #{task} 2>&1") }
-    if $?.success?
-      puts "middleman:build process completed (#{"%.2f" % time}s)"
-    else
-      puts "middleman:build process failed"
-    end
-
   end
 
 end
