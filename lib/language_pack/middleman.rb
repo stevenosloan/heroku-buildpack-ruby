766bac353f4197b5c765fb68e38c877944882e16
Steven Sloan <stevenosloan@gmail.com>
2012-11-21 11:09:00 -0500

    add middleman language pack (lots of cargo culting)

---
+++ lib/language_pack/middleman.rb
@@ -0,0 +1,50 @@
+require "language_pack"
+require "language_pack/rack"
+
+# Middleman Language Pack. Use to generate a middleman site on heroku
+# extending the rack languagepack as we'll use it to serve the built site
+class LanguagePack::Middleman < LanguagePack::Rack
+
+  # detects if this is a Middleman app
+  # @return [Boolean] true if it's a Middleman app
+  def self.use?
+    super && gem_is_bundled?("middleman")
+  end
+
+  def name
+    "Ruby/Rack/Middleman"
+  end
+
+  def compile
+    super
+    allow_git do
+      run_middleman_build_process
+    end
+  end
+
+private
+  
+  # check if a middleman:build rake task exists
+  # or run the default middleman build
+  def run_middleman_build_process
+
+    if rake_task_defined?("middleman:build")
+      task = "rake middleman:build"
+    else
+      puts "no middleman:build task detected"
+      task = "middleman build --clean"
+    end
+
+    require 'benchmark'
+
+    topic "Running: #{task}"
+    time = Benchmark.realtime { pipe("env PATH=$PATH:bin bundle exec #{task} 2>&1") }
+    if $?.success?
+      puts "middleman:build process completed (#{"%.2f" % time}s)"
+    else
+      puts "middleman:build process failed"
+    end
+
+  end
+
+end
