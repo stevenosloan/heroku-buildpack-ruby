f5ce32664f52cfdee26293a9bff5b6b2c2309cd6
Steven Sloan <stevenosloan@gmail.com>
2012-11-21 11:55:48 -0500

    lets try not extending the LanguagePack::Rack class

--- lib/language_pack/middleman.rb
+++ lib/language_pack/middleman.rb
@@ -1,9 +1,9 @@
 require "language_pack"
-require "language_pack/rack"
+require "language_pack/ruby"
 
 # Middleman Language Pack. Use to generate a middleman site on heroku
 # extending the rack languagepack as we'll use it to serve the built site
-class LanguagePack::Middleman < LanguagePack::Rack
+class LanguagePack::Middleman < LanguagePack::Ruby
 
   # detects if this is a Middleman app
   # @return [Boolean] true if it's a Middleman app
@@ -12,7 +12,24 @@ class LanguagePack::Middleman < LanguagePack::Rack
   end
 
   def name
-    "Ruby/Rack/Middleman"
+    "Ruby/Middleman"
+  end
+
+  def default_config_vars
+    super.merge({
+      "RACK_ENV" => "production"
+    })
+  end
+
+  def default_process_types
+    # let's special case thin here if we detect it
+    web_process = gem_is_bundled?("thin") ?
+                    "bundle exec thin start -R config.ru -e $RACK_ENV -p $PORT" :
+                    "bundle exec rackup config.ru -p $PORT"
+
+    super.merge({
+      "web" => web_process
+    })
   end
 
   def compile
@@ -23,6 +40,12 @@ class LanguagePack::Middleman < LanguagePack::Rack
   end
 
 private
+
+  # sets up the profile.d script for this buildpack
+  def setup_profiled
+    super
+    set_env_default "RACK_ENV", "production"
+  end
   
   # check if a middleman:build rake task exists
   # or run the default middleman build
